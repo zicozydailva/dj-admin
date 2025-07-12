77f788e43277d21ed5eab14c3e50ba12e1a71087
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-07-12 23:35:06 +0100

    feat: model setup

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
