3aa85d508ec57f0853886cb2c19ee4e1c599d0dd
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-07-12 23:15:47 +0100

    chore: url mapping

---
+++ firstapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('function', views.hello_world, name='hello_world_function'),
+    path('class', views.HelloWorldView.as_view(), name='hello_world_class'),
+]
