e780378310bf1e6d35f95fc4dc627785d86f0a45
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-07-12 23:09:21 +0100

    chore: functional and class based views

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+def hello_world(request):
+    return HttpResponse("Hello, World!")
+
+class HelloWorldView(View):
+    def get(self, request):
+        return HttpResponse("Hello, World from Class-Based View!")
